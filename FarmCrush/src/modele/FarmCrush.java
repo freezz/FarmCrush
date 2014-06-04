9c37e37c568bac615e28caf88918681922348372
freez <freez@freez-Latitude-E5530-non-vPro>
2014-06-04 17:41:21 +0200

    Nettoyage et contrat FarmCrush,grille et objectif

--- FarmCrush/src/modele/FarmCrush.java
+++ FarmCrush/src/modele/FarmCrush.java
@@ -1,38 +1,86 @@
 package modele;
 
-import com.modeliosoft.modelio.javadesigner.annotations.objid;
+//@
 
-@objid ("eddbc88d-d4a1-4177-b359-c3fcb0738bc9")
+
 public class FarmCrush {
-    @objid ("a38e9223-2912-406b-8171-79520e636a40")
-    private int scoreaActuel;
 
-    @objid ("26266a9b-62c6-4a1b-bf9e-35f42baaa283")
+    private int scoreaActuel;
     private int nbCoupJouer;
 
-    @objid ("89485065-ac05-4e60-a0dd-5faf857d5cce")
+    public Objectif objectif;
     public Grille grille;
 
-    @objid ("b54c4336-0be9-4fb3-90d6-9ea76e9cdcec")
-    public Objectif objectif;
-
-    @objid ("71906a67-c8da-4d21-942c-528e87741740")
+    
+	/**
+	 *   
+	 *   
+	 * @param 
+	 *   
+	 * @required
+	 *  
+	 * @ensure
+	 *  
+	 * @throws 
+	 * 
+	 * @return 
+	 * 	 
+	 */
     public void jouer(final Coordonnee c1, final Coordonnee c2) {
     }
 
-    @objid ("f347e9f0-93fb-4896-b80b-a393962b1f5a")
+	/**
+	 *   
+	 *   
+	 * @param 
+	 *   
+	 * @required
+	 *  
+	 * @ensure
+	 *  
+	 * @throws 
+	 * 
+	 * @return 
+	 * 	 
+	 */
     public void initialisationNiveau() {
     }
 
-    @objid ("4b817065-b2a4-4075-bec3-201eaa0a5664")
+	/**
+	 *   
+	 *   
+	 * @param 
+	 *   
+	 * @required
+	 *  
+	 * @ensure
+	 *  
+	 * @throws 
+	 * 
+	 * @return 
+	 * 	 
+	 */
     public int getScoreActuel() {
-        // TODO Auto-generated return
+
         return 0;
     }
-
-    @objid ("303ad3f7-c6ed-45bf-bd4d-01e0f72fd1f7")
+    
+	/**
+	 *   
+	 *   
+	 * @param 
+	 *   
+	 * @required
+	 *  
+	 * @ensure
+	 *  
+	 * @throws 
+	 * 
+	 * @return 
+	 * 	 
+	 */
     public int getNbCoupJouer() {
-        // TODO Auto-generated return
+
         return 0;
     }
 
--- FarmCrush/src/modele/Grille.java
+++ FarmCrush/src/modele/Grille.java
@@ -2,44 +2,90 @@ package modele;
 
 import java.util.ArrayList;
 import java.util.List;
-import com.modeliosoft.modelio.javadesigner.annotations.objid;
 
-@objid ("869bfca2-7370-46ad-abc4-1445a3c2fca4")
+
+
 public class Grille {
-    @objid ("66694cd1-5f49-407e-a4a9-8a1a7fc3e8f5")
-    public static int TAILLE_LIGNE_MAX = 9;
 
-    @objid ("3ff9f1df-e5a9-48ed-b42e-a16f0f8ea9cd")
+    public static int TAILLE_LIGNE_MAX = 9;
     public static String TAILLE_COLONNE_MAX;
 
-    @objid ("d8475530-d47f-4033-995e-973b28f7f375")
     private int nbColonne;
-
-    @objid ("6f4b13a9-a4ca-436c-8cc4-ba3b9778f727")
     private int nbLigne;
 
-    @objid ("67dcf257-4252-4df1-b31a-0ae1309a37bc")
     public List<Case> cases = new ArrayList<Case> ();
 
-    @objid ("baea0443-71fc-40c6-8a0a-fe3b5b11dfdd")
+	/**
+	 *   
+	 *   
+	 * @param 
+	 *   
+	 * @required
+	 *  
+	 * @ensure
+	 *  
+	 * @throws 
+	 * 
+	 * @return 
+	 * 	 
+	 */
     public void effectuerGraviter() {
     }
 
-    @objid ("447b0445-0b64-4354-9953-9f60d37aeca4")
+	/**
+	 *   
+	 *   
+	 * @param 
+	 *   
+	 * @required
+	 *  
+	 * @ensure
+	 *  
+	 * @throws 
+	 * 
+	 * @return 
+	 * 	 
+	 */
     public boolean checkGrille() {
-        // TODO Auto-generated return
+
         return false;
     }
 
-    @objid ("86d05db0-2b9d-4b79-bab8-40218aa8484d")
+	/**
+	 *   
+	 *   
+	 * @param 
+	 *   
+	 * @required
+	 *  
+	 * @ensure
+	 *  
+	 * @throws 
+	 * 
+	 * @return 
+	 * 	 
+	 */
     public int getNbLigne() {
-        // TODO Auto-generated return
+
         return 0;
     }
 
-    @objid ("44f2e206-7a15-4c81-a3d0-fe634a73db0d")
+	/**
+	 *   
+	 *   
+	 * @param 
+	 *   
+	 * @required
+	 *  
+	 * @ensure
+	 *  
+	 * @throws 
+	 * 
+	 * @return 
+	 * 	 
+	 */
     public int getNbColonne() {
-        // TODO Auto-generated return
+
         return 0;
     }
 
