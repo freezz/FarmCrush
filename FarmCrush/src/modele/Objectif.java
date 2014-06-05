4211ca53cbe6121bffa2522d700244836c550b3c
freez <freez@freez-Latitude-E5530-non-vPro>
2014-06-05 16:21:13 +0200

    lol

--- FarmCrush/src/modele/Objectif.java
+++ FarmCrush/src/modele/Objectif.java
@@ -5,6 +5,8 @@ package modele;
 
 public class Objectif {
 
+	//attributs
+	
     private int score;
     private int nbCoupMax;
     
@@ -21,6 +23,31 @@ public class Objectif {
     public int nbEmballeRestant;
     public int nbMultiRestant;
 
+    
+    //Constructeur
+	/**
+	 * Construit un objectif en mettant tous ces attributs a 0
+	 * 	 
+	 */   
+    public Objectif(){
+        score = 0;
+        nbCoupMax = 0;
+        
+        gelatineRestante = 0;
+
+        nbRougeRestant = 0;
+        nbVioletRestant = 0;
+        nbVertRestant = 0;
+        nbJauneRestant = 0;
+        nbOrangeRestant = 0;
+        nbBleuRestant = 0;
+        
+        nbRayeRestant = 0;
+        nbEmballeRestant = 0;
+        nbMultiRestant = 0;
+    }
+    
+    //Méthodes
 	/**
 	 *   Verifi les differents attributs et retourne si les conditions
 	 *   pour gagner, sont remplis
