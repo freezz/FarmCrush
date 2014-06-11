0739af24c1624b08463b2af5a49e8d09b6057052
uiharu <uiharu@Kazari>
2014-06-11 17:39:34 +0200

    package de test

---
+++ FarmCrush/test/testModele/ObjectifTest.java
@@ -0,0 +1,31 @@
+package testModele;
+
+import static org.junit.Assert.*;
+import modele.Objectif;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ObjectifTest {
+
+   //Attributs
+	protected Objectif obj;
+
+   //Action avant et après l'execution des test
+	@Before
+	public void setUp() throws Exception {
+		obj = new Objectif();		
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
