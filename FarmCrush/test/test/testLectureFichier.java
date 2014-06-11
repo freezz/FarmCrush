3f7bd2f1f47b20406b0bb4326db408a25cf0f77e
freez <freez@freez-Latitude-E5530-non-vPro>
2014-06-11 14:40:42 +0200

    test lecture

---
+++ FarmCrush/test/test/testLectureFichier.java
@@ -0,0 +1,15 @@
+package test;
+
+import modele.*;
+
+public class testLectureFichier {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		FarmCrush boulangere = new FarmCrush();
+		boulangere.initialisationNiveau("annexes/niveau.txt");
+	}
+
+}
