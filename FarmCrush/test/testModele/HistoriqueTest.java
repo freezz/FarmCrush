0dc3e939a6f7369e29cde7bc2a1d046123ffb70e
uiharu <uiharu@Kazari>
2014-06-16 09:59:25 +0200

    Test Gelatine et test historique

---
+++ FarmCrush/test/testModele/HistoriqueTest.java
@@ -0,0 +1,44 @@
+package testModele;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import modele.Coordonnee;
+import modele.Historique;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class HistoriqueTest {
+
+   //Attributs
+	private Historique hist;
+	private Coordonnee coord, coord1;
+	
+	@Before
+	public void setUp() throws Exception {
+		hist = new Historique();
+		coord = new Coordonnee(3, 4);
+		coord1 = new Coordonnee(4, 5);
+		hist.histAdd(coord);
+	}
+
+
+	@Test
+	public void testGetCoordonnees() {
+		assertTrue(hist.getCoordonnees().size() == 1);
+		assertTrue(hist.getCoordonnees() instanceof ArrayList<?>);
+	}
+
+	@Test
+	public void testHistAdd() {
+		assertTrue(hist.histAdd(coord1));
+	}
+	
+	@Test
+	public void comparaison() {
+		assertTrue((hist.getCoordonnees().get(hist.getCoordonnees().size()-1)) == coord1);
+	}
+
+}
