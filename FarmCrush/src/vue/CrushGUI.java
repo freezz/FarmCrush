b434a49a9986d85c2777e687951f115cc1eb9423
jbrougerie <jbrougerie@jbrougerie-Latitude-E5530-non-vPro>
2014-06-16 16:16:39 +0200

    Maj vu

--- FarmCrush/src/vue/CrushGUI.java
+++ FarmCrush/src/vue/CrushGUI.java
@@ -2,6 +2,7 @@ package vue;
 import controleur.*;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Container;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
@@ -18,6 +19,7 @@ import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JProgressBar;
+import javax.swing.JToggleButton;
 
 import modele.Bonbon;
 import modele.Case;
@@ -27,31 +29,31 @@ import modele.FarmCrush;
 public class CrushGUI implements Observer{
 	
 	/* Ensemble des icons */
-	private static final ImageIcon orange = new ImageIcon("img/orange.ico");
-    private static final ImageIcon rouge = new ImageIcon("img/rouge.ico");
-    private static final ImageIcon vert = new ImageIcon("img/vert.ico");
-    private static final ImageIcon bleu = new ImageIcon("img/croix.jpg");
-    private static final ImageIcon violet = new ImageIcon("img/bleu.png");
-    private static final ImageIcon jaune = new ImageIcon("img/bleu.png");
-    private static final ImageIcon orange_raye = new ImageIcon("img/orange_raye.ico");
-    private static final ImageIcon rouge_raye = new ImageIcon("img/rouge_raye.ico");
-    private static final ImageIcon vert_raye = new ImageIcon("img/vert_raye.ico");
-    private static final ImageIcon bleu_raye = new ImageIcon("img/bleu_raye.ico");
-    private static final ImageIcon violet_raye = new ImageIcon("img/violet_raye.ico");
-    private static final ImageIcon jaune_raye = new ImageIcon("img/jaune_raye.ico");
-    private static final ImageIcon orange_emballe = new ImageIcon("img/orange_emballe.ico");
-    private static final ImageIcon rouge_emballe = new ImageIcon("img/rouge_emballe.ico");
-    private static final ImageIcon vert_emballe = new ImageIcon("img/vert_emballe.ico");
-    private static final ImageIcon bleu_emballe = new ImageIcon("img/bleu_emballe.ico");
-    private static final ImageIcon violet_emballe = new ImageIcon("img/violet_emballe.ico");
-    private static final ImageIcon jaune_emballe = new ImageIcon("img/jaune_emballe.ico");
-    private static final ImageIcon multicolore = new ImageIcon("img/multicolore.ico");
+	private static final ImageIcon orange = new ImageIcon("img/orange.png");
+    private static final ImageIcon rouge = new ImageIcon("img/rouge.png");
+    private static final ImageIcon vert = new ImageIcon("img/vert.png");
+    private static final ImageIcon bleu = new ImageIcon("img/bleu.png");
+    private static final ImageIcon violet = new ImageIcon("img/violet.png");
+    private static final ImageIcon jaune = new ImageIcon("img/jaune.png");
+    private static final ImageIcon orange_raye = new ImageIcon("img/orange_raye.png");
+    private static final ImageIcon rouge_raye = new ImageIcon("img/rouge_raye.png");
+    private static final ImageIcon vert_raye = new ImageIcon("img/vert_raye.png");
+    private static final ImageIcon bleu_raye = new ImageIcon("img/bleu_raye.png");
+    private static final ImageIcon violet_raye = new ImageIcon("img/violet_raye.png");
+    private static final ImageIcon jaune_raye = new ImageIcon("img/jaune_raye.png");
+    private static final ImageIcon orange_emballe = new ImageIcon("img/orange_emballe.png");
+    private static final ImageIcon rouge_emballe = new ImageIcon("img/rouge_emballe.png");
+    private static final ImageIcon vert_emballe = new ImageIcon("img/vert_emballe.png");
+    private static final ImageIcon bleu_emballe = new ImageIcon("img/bleu_emballe.png");
+    private static final ImageIcon violet_emballe = new ImageIcon("img/violet_emballe.png");
+    private static final ImageIcon jaune_emballe = new ImageIcon("img/jaune_emballe.png");
+    private static final ImageIcon multicolore = new ImageIcon("img/multicolore.png");
 
     /** Fenêtre principale */
     private JFrame fenetre;
     
     /** Cases du jeu */
-    private JLabel[][] cases ;
+    private JToggleButton[][] cases ;
     
     /** Le modèle - nécessaire pour les controleurs  */
     private FarmCrush modele;
@@ -82,15 +84,16 @@ public class CrushGUI implements Observer{
     	
     	this.modele = m;
     	//Création des cases
-    	cases = new JLabel[m.grille.getLigne()][m.grille.getColonne()];
+    	cases = new JToggleButton[m.grille.getLigne()][m.grille.getColonne()];
     	for (int i = 0 ; i < m.grille.getLigne() ; i++) {
 		    for (int j = 0 ; j < m.grille.getColonne() ; j++) {
-				cases[i][j] = new JLabel();
+				cases[i][j] = new JToggleButton();
 				//attribut public interdit...
-				//cases[i][j].setIcon(contenu2Image(m.grille.getCase(i, j)));
-				cases[i][j].setIcon(new ImageIcon("img/bleu.png"));
-				//GELATINE ??
-				//case[i][j].
+				cases[i][j].setIcon(contenu2Image(m.grille.getCase(i, j)));
+				int coucheGelatine = m.grille.getCase(i, j).getGelatine().getCoucheGelatine();
+				if( coucheGelatine != 0) {
+					cases[i][j].setBackground(new Color(0, 255 / coucheGelatine, 0));
+				}
 				
 				//cases[i][j].addMouseListener(new ClickLabel());
 		    }
@@ -119,40 +122,58 @@ public class CrushGUI implements Observer{
     	JPanel pFenetre = new JPanel(new BorderLayout());
     	
     	/* Les objectifs */
-    	JPanel pObjectifs = new JPanel(new FlowLayout());
+    	JPanel pObjectifs = new JPanel(new GridLayout(11,1));
+    	pObjectifs.setSize(50, 1000);
     	// Ensemble des objectifs //
-    	JLabel lScore = new JLabel("Score :");
-    	score = new JLabel();
-    	score.setIcon(bleu);
-    	JLabel lCoupRestant = new JLabel("Coups Restant :");
-    	coupsRestants = new JLabel(""+m.objectif.getTargetScore());
-    	JLabel lVertRestant = new JLabel("Vert :");
+    	JPanel[] pElementsObjectifs = new JPanel[11];
+    	for(int i = 0 ; i < 11 ; i++) {
+    		pElementsObjectifs[i] = new JPanel(new FlowLayout());
+    		pObjectifs.add(pElementsObjectifs[i]);
+    	}
+    	//score
+    	score = new JLabel(""+m.objectif.getTargetScore());
+    	pElementsObjectifs[0].add(new JLabel("Score :"));
+    	pElementsObjectifs[0].add(score);
+    	//coup restant
+    	coupsRestants = new JLabel(""+m.objectif.getNbCoupMax());
+    	pElementsObjectifs[1].add(new JLabel("Coups Restant :"));
+    	pElementsObjectifs[1].add(coupsRestants);
+    	//vert restant
     	vertRestant = new JLabel(""+m.objectif.getNbVertRestant());
-    	JLabel lRougeRestant = new JLabel("Rouge :");
+    	pElementsObjectifs[2].add(new JLabel("Vert :"));
+    	pElementsObjectifs[2].add(vertRestant);
+    	//
     	rougeRestant = new JLabel(""+m.objectif.getNbRougeRestant());
-    	JLabel lBleuRestant = new JLabel("Bleu :");
+    	pElementsObjectifs[3].add(new JLabel("Rouge :"));
+    	pElementsObjectifs[3].add(rougeRestant);
+    	//
     	bleuRestant = new JLabel(""+m.objectif.getNbBleuRestant());
-    	JLabel lVioletRestant = new JLabel("Violet :");
+    	pElementsObjectifs[4].add(new JLabel("Bleu :"));
+    	pElementsObjectifs[4].add(bleuRestant);
+    	//
     	violetRestant = new JLabel(""+m.objectif.getNbVioletRestant());
-    	JLabel lJauneRestant = new JLabel("Jaune :");
+    	pElementsObjectifs[5].add(new JLabel("Violet :"));
+    	pElementsObjectifs[5].add(violetRestant);
+    	//
     	jauneRestant = new JLabel(""+m.objectif.getNbJauneRestant());
-    	JLabel lOrangeRestant = new JLabel("Orange :");
+    	pElementsObjectifs[6].add(new JLabel("Jaune :"));
+    	pElementsObjectifs[6].add(jauneRestant);
+    	//
     	orangeRestant = new JLabel(""+m.objectif.getNbOrangeRestant());
-    	JLabel lEmballeRestant = new JLabel("Emballé :");
+    	pElementsObjectifs[7].add(new JLabel("Orange :"));
+    	pElementsObjectifs[7].add(orangeRestant);
+    	//
     	emballeRestant = new JLabel(""+m.objectif.getNbEmballeRestant());
-    	JLabel lRayeRestant = new JLabel("Rayé :");
+    	pElementsObjectifs[8].add(new JLabel("Emballé :"));
+    	pElementsObjectifs[8].add(emballeRestant);
+    	//
     	rayeRestant = new JLabel(""+m.objectif.getNbRayeRestant());
-    	JLabel lMultiRestant = new JLabel("Multicolor :");
+    	pElementsObjectifs[9].add(new JLabel("Rayé :"));
+    	pElementsObjectifs[9].add(rayeRestant);
+    	//
     	multicolorRestant = new JLabel(""+m.objectif.getNbMultiRestant());
-    	pObjectifs.add(lScore);
-    	pObjectifs.add(score);
-    	pObjectifs.add(lCoupRestant);
-    	pObjectifs.add(coupsRestants);
-    	/*pObjectifs.add(...);
-    	pObjectifs.add(...);
-    	pObjectifs.add(...);
-    	pObjectifs.add(...);
-    	pObjectifs.add(...);*/
+    	pElementsObjectifs[10].add(new JLabel("Multicolor :"));
+    	pElementsObjectifs[10].add(multicolorRestant);
     	
 		
 		/* La grille */
@@ -170,7 +191,7 @@ public class CrushGUI implements Observer{
 		
 		pFenetre.add(barreMenu, BorderLayout.NORTH);
 		pFenetre.add(pGrille, BorderLayout.CENTER);
-		//pFenetre.add(pObjectifs, BorderLayout.WEST);
+		pFenetre.add(pObjectifs, BorderLayout.WEST);
 		
 		contenu.add(pFenetre);
 		
@@ -222,6 +243,7 @@ public class CrushGUI implements Observer{
 	@Override
 	public void update(Observable arg0, Object arg1) {
 		// TODO Auto-generated method stub
+		
 		
 	}
 
