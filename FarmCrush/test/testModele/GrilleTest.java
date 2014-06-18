72afc5568f669cde9c9b840ce13eda26689abebc
uiharu <uiharu@Kazari>
2014-06-18 17:05:14 +0200

    test grille

---
+++ FarmCrush/test/testModele/GrilleTest.java
@@ -0,0 +1,99 @@
+package testModele;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class GrilleTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testGrille() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInitialiser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetColonne() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetLigne() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTableau() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCase() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetColonne() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetLigne() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRetirerBonbonCase() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testBonbonNull() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPositionBonbon() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSupprimerBonbonCase() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEffectuerGraviter() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFaireDescendreBonbon() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCheckGrille() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCheckInteraction() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDetruireBonbonExistant() {
+		fail("Not yet implemented");
+	}
+
+}
