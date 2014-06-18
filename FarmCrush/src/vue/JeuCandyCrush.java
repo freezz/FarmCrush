2c18002cb1ff5ede94aa4c35c16f386b74ea480f
uiharu <uiharu@Kazari>
2014-06-18 17:30:26 +0200

    test grille in progress

--- FarmCrush/src/vue/JeuCandyCrush.java
+++ FarmCrush/src/vue/JeuCandyCrush.java
@@ -6,7 +6,7 @@ public class JeuCandyCrush {
 
 	public static void main(String[] args) {
 
-		FarmCrush modele = new FarmCrush("annexes/niveauTest1.txt");
+		FarmCrush modele = new FarmCrush("annexes/lvltest.txt");
 		
 		CrushGUI vue = new CrushGUI(modele);
        	modele.addObserver(vue);
