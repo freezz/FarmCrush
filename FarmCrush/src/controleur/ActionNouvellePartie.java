9c0709f379f3f2976a20b282e36ab5cf83034906
jbrougerie <jbrougerie@jbrougerie-Latitude-E5530-non-vPro>
2014-06-19 13:17:31 +0200

    Modif vue et controler

---
+++ FarmCrush/src/controleur/ActionNouvellePartie.java
@@ -0,0 +1,13 @@
+package controleur;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ActionNouvellePartie implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		
+	}
+
+}
